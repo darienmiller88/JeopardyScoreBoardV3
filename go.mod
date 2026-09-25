08d3ffcfa7a2942a0735d1d4fc71a21b939c73fc
darie <darienmiller88@yahoo.com>
2026-09-25 16:21:56 -0400

    refactor: moved css and js into static folder

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module JeopardyScoreBoardV3
 
 go 1.26.1
 
-require github.com/go-chi/chi/v5 v5.3.2 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.3.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main(){
+	godotenv.Load()
+
 	app := chi.NewRouter()
 	
 	app.Use(middleware.Logger)
 
 	t := template.Must(template.ParseGlob("*.html"))
+
+	fs := http.FileServer(http.Dir("static"))
+	app.Handle("/static/*", http.StripPrefix("/static/", fs))
 
 	app.Get("/", func(res http.ResponseWriter, req *http.Request) {
 		if err := t.ExecuteTemplate(res, "index.html", nil); err != nil{
@@ -21,5 +29,6 @@ func main(){
 		}
 	})
 
-	http.ListenAndServe(":3000", app)
+	fmt.Println("Running on port:", os.Getenv("PORT"))
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), app)
 }
